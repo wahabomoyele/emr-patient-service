76abd58a07abab6e6ed7c390847fd2ef4d267235
wahabomoyele <wahabomoyele@yahoo.com>
2021-12-19 18:08:39 +0100

    Updated TestData class

--- src/test/java/com/orm/emrpatientservice/Util/TestData.java
+++ src/test/java/com/orm/emrpatientservice/Util/TestData.java
@@ -1,6 +1,10 @@
 package com.orm.emrpatientservice.Util;
 
+import com.orm.emrpatientservice.data.constant.State;
+import com.orm.emrpatientservice.data.entity.Address;
 import com.orm.emrpatientservice.data.entity.Patient;
+import com.orm.emrpatientservice.data.form.AddressForm;
+import com.orm.emrpatientservice.data.form.NextOfKinForm;
 import com.orm.emrpatientservice.data.form.PatientForm;
 
 import java.util.ArrayList;
@@ -11,8 +15,11 @@ import java.util.UUID;
 public class TestData {
     public final UUID uuid = UUID.randomUUID();
     public final PatientForm patientForm = new PatientForm("Mr", "Wahab", "Omoyele", "Akorede", "wahabomoyele@yahoo.com", "+2347068502356", "06/09/1990", "male", new HashSet<>(Arrays.asList(1L,2L)));
-    public Patient patient;
+    public final AddressForm addressForm = new AddressForm("18B, Mayegun Street", "Abeokuta", "NG_OG");
+    public final NextOfKinForm nextOfKinForm = new NextOfKinForm("mr", "samuel", "badmus", null, "samuelbadmus@gmail.com", "09037282618", "Olabode close, Lekki, Lagos");
+    public final Address address = new Address("19B, Mayegun Street", "Abeokuta", State.NG_OG);
+    public Patient patient = new Patient();
     public TestData() {
-        patient = Patient.build(patientForm, new Patient());
+        Patient.build(patientForm, patient);
     }
 }
