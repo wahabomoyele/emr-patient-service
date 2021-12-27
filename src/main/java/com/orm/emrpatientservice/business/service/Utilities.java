a7690e8305fbf93e50b3b8055ff2ad624df1990a
wahabomoyele <wahabomoyele@yahoo.com>
2021-12-27 11:06:54 +0100

    Edit on Utilities class - Added parseDateTime method

--- src/main/java/com/orm/emrpatientservice/business/service/Utilities.java
+++ src/main/java/com/orm/emrpatientservice/business/service/Utilities.java
@@ -3,6 +3,7 @@ package com.orm.emrpatientservice.business.service;
 import com.orm.emrpatientservice.business.exception.BadRequestException;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
@@ -11,7 +12,7 @@ import java.util.UUID;
 public class Utilities {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy", new Locale("en"));
     private static final DateTimeFormatter DATE_FORMAT2 = DateTimeFormatter.ofPattern("yyyy/MM/dd", new Locale("en"));
-
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyy/MM/dd HH:mm:ss", new Locale("en"));
 
     public static UUID getId(String uuidString) {
         try {
@@ -33,5 +34,13 @@ public class Utilities {
             }
         }
         return date;
+    }
+
+    public static LocalDateTime parseDateTime(String dateTimeString) {
+        try {
+            return LocalDateTime.parse(dateTimeString, DATE_TIME_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new BadRequestException("Invalid date-time format. Use yyy/MM/dd HH:mm:ss");
+        }
     }
 }
