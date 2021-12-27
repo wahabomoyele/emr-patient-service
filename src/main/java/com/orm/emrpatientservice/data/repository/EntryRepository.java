6625271d940e16323a3718a151b2a38406cf27b5
wahabomoyele <wahabomoyele@yahoo.com>
2021-12-27 11:01:49 +0100

    Edit on EntryRepository - Added new method findByPatient

--- src/main/java/com/orm/emrpatientservice/data/repository/EntryRepository.java
+++ src/main/java/com/orm/emrpatientservice/data/repository/EntryRepository.java
@@ -2,10 +2,14 @@ package com.orm.emrpatientservice.data.repository;
 
 import com.orm.emrpatientservice.data.entity.Patient;
 import com.orm.emrpatientservice.data.entity.entry.Entry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface EntryRepository extends JpaRepository<Entry, UUID> {
     Integer countAllByPatient(Patient patient);
+
+    Page<Entry> findAllByPatient(Patient patient, Pageable pageable);
 }
